c2649a67ef04f737f45403b7c63dcbbaa228ab99
lithops-zty <1220708797@qq.com>
2024-09-27 00:49:55 +0800

    Make remark optional in add command

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -5,13 +5,9 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 
 public class Remark {
 
-    public static final String MESSAGE_CONSTRAINTS = "Remarks can take any values, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Remarks can take any values, or be left blank";
 
-    /*
-     * The first character of the remark must not be a whitespace,
-     * otherwise " " (a blank string) becomes a valid input.
-     */
-    public static final String VALIDATION_REGEX = "[^\\s].*";
+    public static final String VALIDATION_REGEX = ".*";
 
     public final String value;
 
