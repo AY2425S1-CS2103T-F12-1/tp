3b841c556bc08847dbaafc86796603347e8da562
Jiaxi <jx98069964@gmail.com>
2024-10-17 23:47:13 +0800

    Fix test bug

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -37,8 +37,8 @@ public class TypicalPersons {
             .withPhone("94351253")
             .withTags("friends")
             .withModuleRoleMap(
-                    new ModuleCode[] {new ModuleCode("CS1231S")},
-                    new RoleType[] {RoleType.TUTOR}).build();
+                    new ModuleCode[] {new ModuleCode("CS1101S")},
+                    new RoleType[] {RoleType.STUDENT}).build();
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25")
             .withEmail("johnd@example.com").withPhone("98765432")
